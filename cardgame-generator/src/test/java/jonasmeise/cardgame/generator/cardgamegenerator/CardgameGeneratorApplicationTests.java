1f64d1a394ea8a3e06c569a64b7a6881a5d891e1
jonaspmeise <62212517+jonaspmeise@users.noreply.github.com>
2022-11-10 14:03:29 +0100

    Intial commit

---
+++ cardgame-generator/src/test/java/jonasmeise/cardgame/generator/cardgamegenerator/CardgameGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package jonasmeise.cardgame.generator.cardgamegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardgameGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
